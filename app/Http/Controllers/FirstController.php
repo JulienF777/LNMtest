8acfa45efb7f5a32f0b904d153a7ba912363b84d
aserlite <arthurcuvillon691@gmail.com>
2022-10-19 12:42:31 +0200

    patch bug case vide login

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -13,20 +13,18 @@ class FirstController extends Controller
     }
     
     function loginT(){
-        //$films = DB::select('select * from film');
             if (!isset($_POST['mail']) || !isset($_POST['pwd'])) {
-              return redirect('/login');
+              return redirect('/index');
               } else {
                 var_dump($_POST["pwd"]);
                 $vars=[$_POST["mail"],$_POST["pwd"]];
                 $user = DB::select('select * from user where email=? and mdp=sha1(?)',[$_POST['mail'],$_POST['pwd']]); 
-                $user = $user[0];
 
-                if ($user === false) {
+                if (!isset($user[0])) {
                   return redirect('/login');
                 } else {
-                  session(["id" => $user->id]);
-                  session(["login" => $user->login]);
+                  session(["id" => $user[0]->id]);
+                  session(["login" => $user[0]->login]);
                   if (isset($_POST['rememberme'])) {
                       $token = bin2hex(random_bytes(20));
                       setcookie('token', $token, time() + 86400 );
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,8 +2,8 @@
   <div class="login">
     <form action='loginT' method='POST'>
         @csrf
-      <input type='mail' name='mail' placeholder='e-mail'>
-      <input type='password' name='pwd' placeholder='Mot de passe'>
+      <input type='mail' name='mail' placeholder='e-mail' required>
+      <input type='password' name='pwd' placeholder='Mot de passe' required>
 
       <label class="rememberme" for="rememberme"><input type="checkbox" id="rememberme" name="rememberme" value="rememberme"> Se souvenir de moi</label>
       <input type='submit' name='connecter' value='Connexion'>
